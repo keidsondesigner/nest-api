bc56dd6a6fb2f176433b988f7aa4f16424b53e6c
keidsondesigner <keidsondesigner@gmail.com>
2023-09-13 14:58:04 -0400

    feat: :sparkles: implementação GET listar USERS

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,19 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('users')
 export class UserController {
   @Post()
   async create(@Body() body) {
     return { body };
+  }
+
+  @Get()
+  async findMany() {
+    return { users: [] };
+  }
+
+  @Get(':id')
+  async findUnique(@Param() param) {
+    return { user: {}, param };
   }
 }
